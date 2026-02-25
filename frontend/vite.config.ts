6b5e67886c533c001f1617bf9e008deb83ea8559
Harikrishnan Santhosh <harikrishnansanthosh06@gmail.com>
2026-02-25 21:22:21 +0530

    build : Migrated to react 19 and tailwind

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'], // Include .JPG files as assets
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     port: 5173,
     strictPort: true,
